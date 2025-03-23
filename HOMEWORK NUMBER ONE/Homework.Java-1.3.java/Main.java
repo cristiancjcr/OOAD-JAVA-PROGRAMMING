90582f72b44db8b6d0549465f1b3fad41d4406a2
cristian cojocaru <cojocaru.florin.n7f@student.ucv.ro>
2025-03-23 22:55:03 +0200

    file

---
+++ HOMEWORK NUMBER ONE/Homework.Java-1.3.java/Main.java
@@ -0,0 +1,10 @@
+// clasa principala, ce contine metoda main()
+public class Main {
+    public static void main(String[] args) {
+        // cream un obiect de tip persoana
+        Persoana p = new Persoana("Alex", 30, "Bucuresti");
+
+        // afisam obiectul
+        System.out.println(p);
+    }
+}
