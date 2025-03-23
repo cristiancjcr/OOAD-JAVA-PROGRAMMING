8a5503c5247af2e0740838fa0f2265096c256d0b
cristian cojocaru <cojocaru.florin.n7f@student.ucv.ro>
2025-03-23 23:29:38 +0200

    file

---
+++ HOMEWORK NUMBER TWO/Homework.Java-1.18.java/SortList.java
@@ -0,0 +1,11 @@
+// SortList.java
+import java.util.Collections;
+import java.util.List;
+
+public class SortList {
+    // Metodă pentru sortarea listei de numere întregi
+    public static void sort(List<Integer> numbers) {
+        // Sortăm lista folosind Collections.sort()
+        Collections.sort(numbers);
+    }
+}
