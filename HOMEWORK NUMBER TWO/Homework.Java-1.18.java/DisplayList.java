953731364228fc83d61f3cd495fb9a93fe930f71
cristian cojocaru <cojocaru.florin.n7f@student.ucv.ro>
2025-03-23 23:30:08 +0200

    file

---
+++ HOMEWORK NUMBER TWO/Homework.Java-1.18.java/DisplayList.java
@@ -0,0 +1,9 @@
+// DisplayList.java
+import java.util.List;
+
+public class DisplayList {
+    // Metodă pentru afișarea listei
+    public static void display(List<Integer> numbers) {
+        System.out.println("Lista: " + numbers);
+    }
+}
