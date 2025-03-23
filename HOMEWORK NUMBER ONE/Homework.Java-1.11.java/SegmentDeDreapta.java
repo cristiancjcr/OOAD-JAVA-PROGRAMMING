5a525e67cf721b8d72787707d797baeda28a31b5
cristian cojocaru <cojocaru.florin.n7f@student.ucv.ro>
2025-03-23 22:26:32 +0200

    test

---
+++ HOMEWORK NUMBER ONE/Homework.Java-1.11.java/SegmentDeDreapta.java
@@ -0,0 +1,23 @@
+public class SegmentDeDreapta implements Shape {
+    private String culoare;
+    private double lungime;
+
+    public SegmentDeDreapta(String culoare, double lungime) {
+        this.culoare = culoare;
+        this.lungime = lungime;
+    }
+
+    @Override
+    public String getCuloare() {
+        return culoare;
+    }
+
+    @Override
+    public double getPerimetru() {
+        return lungime;
+    }
+
+    public double getLungime() {
+        return lungime;
+    }
+}
