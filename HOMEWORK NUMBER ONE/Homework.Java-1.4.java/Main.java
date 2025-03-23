f0b1b96eb40713f2f126d848c9660b5fc18212f8
cristian cojocaru <cojocaru.florin.n7f@student.ucv.ro>
2025-03-23 22:58:10 +0200

    file

---
+++ HOMEWORK NUMBER ONE/Homework.Java-1.4.java/Main.java
@@ -0,0 +1,9 @@
+// metoda main() - punctul de start
+public class Main {
+    public static void main(String[] args) {
+        Caine caine = new Caine("Rex");
+        caine.move();
+        caine.eat();
+        caine.sleep();
+    }
+}
