ab78734ff2b7e4d3d715c1a30a12abc0261e8434
cristian cojocaru <cojocaru.florin.n7f@student.ucv.ro>
2025-03-24 20:32:26 +0200

    file

---
+++ HOMEWORK NUMBER TWO/Maven Application - 2 unit tests/Calculator.java
@@ -0,0 +1,27 @@
+package com.example;
+
+/**
+ * Clasa Calculator oferă metode pentru operații matematice simple.
+ */
+public class Calculator {
+
+    /**
+     * Adună două numere întregi.
+     * @param a Primul număr
+     * @param b Al doilea număr
+     * @return Suma lui a și b
+     */
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    /**
+     * Înmulțește două numere întregi.
+     * @param a Primul număr
+     * @param b Al doilea număr
+     * @return Produsul lui a și b
+     */
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+}
