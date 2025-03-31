4a9ae2b22a0b82cf2f850693d3a2ad3ca96409fd
cristian cojocaru <cojocaru.florin.n7f@student.ucv.ro>
2025-03-31 21:57:26 +0300

    file

---
+++ HOMEWORK NUMBER THREE/Facade Pattern/Projector.java
@@ -0,0 +1,14 @@
+// Fișier: Projector.java
+public class Projector {
+    public void turnOn() {
+        System.out.println("Proiector pornit.");
+    }
+
+    public void setInput(String source) {
+        System.out.println("Proiector setat pe: " + source);
+    }
+
+    public void turnOff() {
+        System.out.println("Proiector oprit.");
+    }
+}
