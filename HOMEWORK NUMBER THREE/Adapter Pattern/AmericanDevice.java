20e93a27640afe84fbae0c735111ae7d7373fa00
cristian cojocaru <cojocaru.florin.n7f@student.ucv.ro>
2025-03-31 21:31:59 +0300

    file

---
+++ HOMEWORK NUMBER THREE/Adapter Pattern/AmericanDevice.java
@@ -0,0 +1,6 @@
+// Fișier: AmericanDevice.java
+public class AmericanDevice {
+    public void plugInAmericanSocket() {
+        System.out.println("Dispozitiv american conectat la priză americană.");
+    }
+}
