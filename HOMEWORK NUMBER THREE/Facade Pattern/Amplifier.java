8b9b3de6089a98f552327c8b8fe7c9703e439edf
cristian cojocaru <cojocaru.florin.n7f@student.ucv.ro>
2025-03-31 21:57:07 +0300

    file

---
+++ HOMEWORK NUMBER THREE/Facade Pattern/Amplifier.java
@@ -0,0 +1,14 @@
+// Fișier: Amplifier.java
+public class Amplifier {
+    public void turnOn() {
+        System.out.println("Amplificator pornit.");
+    }
+
+    public void setVolume(int level) {
+        System.out.println("Volum setat la: " + level);
+    }
+
+    public void turnOff() {
+        System.out.println("Amplificator oprit.");
+    }
+}
