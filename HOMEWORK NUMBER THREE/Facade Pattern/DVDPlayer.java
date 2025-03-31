2b94a468027a813c0cf8f07eafaab1880ccdf54b
cristian cojocaru <cojocaru.florin.n7f@student.ucv.ro>
2025-03-31 21:56:53 +0300

    file

---
+++ HOMEWORK NUMBER THREE/Facade Pattern/DVDPlayer.java
@@ -0,0 +1,14 @@
+// Fișier: DVDPlayer.java
+public class DVDPlayer {
+    public void turnOn() {
+        System.out.println("DVD Player pornit.");
+    }
+
+    public void play(String movie) {
+        System.out.println("Redare film: " + movie);
+    }
+
+    public void turnOff() {
+        System.out.println("DVD Player oprit.");
+    }
+}
