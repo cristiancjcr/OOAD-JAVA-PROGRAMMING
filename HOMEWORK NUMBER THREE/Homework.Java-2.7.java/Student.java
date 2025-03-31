44e969cd706db15bb5381a4c145b23ef23d78c2c
cristian cojocaru <cojocaru.florin.n7f@student.ucv.ro>
2025-03-31 22:58:58 +0300

    file

---
+++ HOMEWORK NUMBER THREE/Homework.Java-2.7.java/Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    private String nume;
+
+    // Constructor pentru crearea unui student cu numele
+    public Student(String nume) {
+        this.nume = nume;
+    }
+
+    // Getter pentru numele studentului
+    public String getNume() {
+        return nume;
+    }
+}
