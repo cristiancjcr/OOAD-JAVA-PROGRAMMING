c13cf9a7cec4b257b0a9eb60f949df8724d92b95
cristian cojocaru <cojocaru.florin.n7f@student.ucv.ro>
2025-03-31 22:54:48 +0300

    file

---
+++ HOMEWORK NUMBER THREE/Homework.Java-2.5.java/C2Nr.java
@@ -0,0 +1,15 @@
+public class C2Nr {
+    private double num1;
+    private double num2;
+
+    // Constructor care primește două numere
+    public C2Nr(double num1, double num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    // Metodă pentru calcularea sumei celor două numere
+    public double suma() {
+        return num1 + num2;
+    }
+}
