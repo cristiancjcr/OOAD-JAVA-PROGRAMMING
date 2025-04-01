ec3d6b3625229b8a5dd3bd9f6e61ca1f052be0e9
cristian cojocaru <cojocaru.florin.n7f@student.ucv.ro>
2025-04-01 22:29:58 +0300

    file

---
+++ HOMEWORK NUMBER THREE/Factory Pattern/Car.java
@@ -0,0 +1,7 @@
+// Car.java
+public class Car implements Vehicle {
+    @Override
+    public void drive() {
+        System.out.println("Driving a car.");  // Se afișează mesajul specific pentru un car
+    }
+}
