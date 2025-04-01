6674b704d891aa0dc9ae099a8ded9845fd81f60a
cristian cojocaru <cojocaru.florin.n7f@student.ucv.ro>
2025-04-01 22:30:15 +0300

    file

---
+++ HOMEWORK NUMBER THREE/Factory Pattern/Truck.java
@@ -0,0 +1,7 @@
+// Truck.java
+public class Truck implements Vehicle {
+    @Override
+    public void drive() {
+        System.out.println("Driving a truck.");  // Se afișează mesajul specific pentru un truck
+    }
+}
