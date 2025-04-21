4a1857823e65df36445d9e081d2ddaeb029a5304
cristian cojocaru <cojocaru.florin.n7f@student.ucv.ro>
2025-04-21 22:46:14 +0300

    file

---
+++ HOMEWORK NUMBER FOUR/Homework.Java-3.4.java/SimplePrinter.java
@@ -0,0 +1,7 @@
+// Clasă care implementează doar funcția de printare
+public class SimplePrinter implements Printer {
+    @Override
+    public void print() {
+        System.out.println("Printing document...");
+    }
+}
