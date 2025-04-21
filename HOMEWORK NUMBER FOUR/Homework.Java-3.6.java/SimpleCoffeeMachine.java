28c6ac78839dabbc0337eaf2471920655678cb23
cristian cojocaru <cojocaru.florin.n7f@student.ucv.ro>
2025-04-21 23:05:55 +0300

    file

---
+++ HOMEWORK NUMBER FOUR/Homework.Java-3.6.java/SimpleCoffeeMachine.java
@@ -0,0 +1,7 @@
+// Mașină de cafea simplă
+public class SimpleCoffeeMachine implements CoffeeMachine {
+    @Override
+    public void start() {
+        System.out.println("Simple Coffee Machine: Brewing a classic coffee...");
+    }
+}
