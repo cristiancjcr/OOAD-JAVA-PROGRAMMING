6fe14f8a156f2e8f0f4e187878b69f74eac34844
cristian cojocaru <cojocaru.florin.n7f@student.ucv.ro>
2025-04-21 22:21:47 +0300

    file

--- HOMEWORK NUMBER FOUR/Homework.Java-3.1.java/EquationResult.java
+++ HOMEWORK NUMBER FOUR/Homework.Java-3.1.java/EquationResult.java
@@ -1,9 +1,11 @@
+// Clasa care modelează rezultatul unei ecuații de gradul I
 public class EquationResult {
-    public double a;
-    public double b;
-    public String type; // "One Solution", "No Solution", "Infinite Solutions"
-    public Double solution; // null dacă nu există soluție unică
+    public double a; // Coeficientul lui x
+    public double b; // Termenul liber
+    public String type; // Tipul soluției: "One Solution", "No Solution", "Infinite Solutions"
+    public Double solution; // Soluția calculată (sau null dacă nu există una unică)
 
+    // Constructor pentru inițializarea câmpurilor
     public EquationResult(double a, double b, String type, Double solution) {
         this.a = a;
         this.b = b;
