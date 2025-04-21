ffbef2b3ac7467001b8a378920d4d0241eb2155d
cristian cojocaru <cojocaru.florin.n7f@student.ucv.ro>
2025-04-21 22:53:23 +0300

    file

---
+++ HOMEWORK NUMBER FOUR/Homework.Java-3.5.java/Main.java
@@ -0,0 +1,7 @@
+// Clasa principală care rulează aplicația
+public class Main {
+    public static void main(String[] args) {
+        CoffeeLover user = new CoffeeLover();
+        user.makeCoffee(); // Rulează mașinile create intern
+    }
+}
