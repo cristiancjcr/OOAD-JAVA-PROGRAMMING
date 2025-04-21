bfdec0ac6d35c358a5330820ee70cc5c283c9ff0
cristian cojocaru <cojocaru.florin.n7f@student.ucv.ro>
2025-04-21 23:06:38 +0300

    file

---
+++ HOMEWORK NUMBER FOUR/Homework.Java-3.6.java/ComplexCoffeeMachine.java
@@ -0,0 +1,7 @@
+// Mașină de cafea complexă
+public class ComplexCoffeeMachine implements CoffeeMachine {
+    @Override
+    public void start() {
+        System.out.println("Complex Coffee Machine: Preparing cappuccino with foam and flavor...");
+    }
+}
