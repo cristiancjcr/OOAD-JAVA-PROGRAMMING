3070a9384178709d98ca474dd32196cdef35a3da
cristian cojocaru <cojocaru.florin.n7f@student.ucv.ro>
2025-04-21 22:45:54 +0300

    file

---
+++ HOMEWORK NUMBER FOUR/Homework.Java-3.4.java/MultifunctionalPrinter.java
@@ -0,0 +1,4 @@
+// Interfață care moștenește toate celelalte interfețe
+public interface MultifunctionalPrinter extends Printer, Scanner, Fax {
+    // Moștenește automat metodele: print(), scan(), fax()
+}
