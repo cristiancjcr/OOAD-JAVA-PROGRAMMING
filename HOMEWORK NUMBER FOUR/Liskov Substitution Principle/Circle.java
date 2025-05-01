1b8a85a912dc103c67f8137bad9ea98ed5ddcec3
cristian cojocaru <cojocaru.florin.n7f@student.ucv.ro>
2025-05-01 21:51:55 +0300

    file

---
+++ HOMEWORK NUMBER FOUR/Liskov Substitution Principle/Circle.java
@@ -0,0 +1,13 @@
+// Clasa derivată Circle care moștenește Shape
+public class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+}
