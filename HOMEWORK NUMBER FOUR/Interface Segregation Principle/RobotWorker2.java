f2ea5a2590adeaa9a4eb8085c829c4a48d59fe80
cristian cojocaru <cojocaru.florin.n7f@student.ucv.ro>
2025-05-01 22:04:11 +0300

    file

---
+++ HOMEWORK NUMBER FOUR/Interface Segregation Principle/RobotWorker2.java
@@ -0,0 +1,7 @@
+// Implementarea Worker pentru roboți, care implementează doar interfața Workable
+public class RobotWorker implements Workable {
+    @Override
+    public void work() {
+        System.out.println("Robot is working");
+    }
+}
