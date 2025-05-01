425efca3cea256c0d9bef3a4c6dc145f90f16de9
cristian cojocaru <cojocaru.florin.n7f@student.ucv.ro>
2025-05-01 22:03:53 +0300

    file

---
+++ HOMEWORK NUMBER FOUR/Interface Segregation Principle/HumanWorker2.java
@@ -0,0 +1,12 @@
+// Implementarea Worker pentru oameni, care implementează ambele interfețe
+public class HumanWorker implements Workable, Eatable {
+    @Override
+    public void work() {
+        System.out.println("Human is working");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Human is eating");
+    }
+}
